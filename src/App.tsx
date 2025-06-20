8009cd017b780f28c924365084cd7006f9b6184e
alexsia08 <nastutulka@gmail.com>
2025-06-21 04:26:58 +0700

    1

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,26 @@
 import React from "react";
 import { BrowserRouter } from "react-router";
 import { CSSBoundingContainer } from "./shared/styles";
-import { useTranslations } from "next-intl";
+// import { useTranslations } from "next-intl";
 import { Typography } from "antd";
 import { CSSTitle, CSSTextP } from "./styles";
 
 const { Title, Paragraph } = Typography;
 
 export const App: React.FC = () => {
-  const tHomePage = useTranslations(`home-page`);
+  // const tHomePage = useTranslations(`home-page`);
 
   return (
     <BrowserRouter basename="/new_task">
       <CSSBoundingContainer>
         <Title level={2} style={CSSTitle}>
-          {tHomePage("title")}
+          Timed Color Cards
         </Title>
-        <Paragraph style={CSSTextP}>{tHomePage("text")}</Paragraph>
+        <Paragraph style={CSSTextP}>
+          Для начала задайте количество карточек и интервал. Карточки
+          генерируются автоматически. Если клинуть на карточку, то таймер
+          обновится, иначе она исчезнет когда время истечёт
+        </Paragraph>
       </CSSBoundingContainer>
     </BrowserRouter>
   );
